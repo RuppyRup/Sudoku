988c2b2a7610c20423cfe9ecb1b0a0a851eb5500
RuppyRup <rupert.waldron@yahoo.co.uk>
2017-08-01 14:03:48 +0100

    Initial Commit

---
+++ Sudoku/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sudoku
+//
+//  Created by Rupert Waldron on 01/08/2017.
+//  Copyright © 2017 RuppyRup. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
